72faa6ee1fefd709805924af24988d450694005e
Sroopesh1122 <shivuroopesh6362@gmail.com>
2024-11-28 10:08:28 +0530

    Dao added

--- src/main/java/com/foodApp/Dao/UserDao.java
+++ src/main/java/com/foodApp/Dao/UserDao.java
@@ -1,6 +1,11 @@
 package com.foodApp.Dao;
 
+import com.foodApp.Dto.User;
+
 public interface UserDao
 {
-  
+  public User add(User u);
+  public User update(User u);
+  public User getOne(int userId);
+  public boolean delete(int userId);
 }
